9430ff89ba0596ef3f7b069bcf99b3cb740a8c35
NadaHm444 <161245560+NadaHm444@users.noreply.github.com>
2024-03-09 02:11:49 +0300

    Update TestJewelryStore.java

--- project113/src/project113/TestJewelryStore.java
+++ project113/src/project113/TestJewelryStore.java
@@ -3,6 +3,7 @@ package project113;
 public class TestJewelryStore {
 
 	public static void main(String[] args) {
+		//rrr
 		// TODO Auto-generated method stub
 System.out.print("first sentence");
 	}
