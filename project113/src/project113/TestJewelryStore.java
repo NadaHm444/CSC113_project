3182020d1467294a2dbd535d54dcbcee699684d6
NadaHm444 <161245560+NadaHm444@users.noreply.github.com>
2024-03-10 01:55:28 +0300

    Update TestJewelryStore.java

--- project113/src/project113/TestJewelryStore.java
+++ project113/src/project113/TestJewelryStore.java
@@ -18,6 +18,42 @@ public class TestJewelryStore {
 	     System.out.println("4. Display a customer order");
 	     System.out.println("5. Display today's orderes");
 	     System.out.println("6. Exit");
+             ch= input.nextInt();
+       switch(ch){
+               Case 1:
+	        System.out.println("----------Add order----------");
+	       System.out.println("your choice is : 1");
+	       System.out.println("Enter : customer name, customer id, jewelry price and order id. ");
+		String CustomerName=inut.next();
+	        
+
+	       break;
+
+		Case 2:
+
+
+		break;
+                Case 3:
+
+	       break;
+
+		Case 4:
+
+
+		break;
+                 Case 5:
+
+	       break;
+
+		Case 6:
+
+
+		break;
+
+
+       }
+
+			
 		}while(ch!=6);
     
 		
