8a0e501ddaba875a7071dc4b1e85e39f088cb958
NadaHm444 <161245560+NadaHm444@users.noreply.github.com>
2024-03-10 03:35:19 +0300

    Update TestJewelryStore.java

--- project113/src/project113/TestJewelryStore.java
+++ project113/src/project113/TestJewelryStore.java
@@ -73,7 +73,7 @@ public class TestJewelryStore {
 
 	       foundOrder=Order OurStore.searchOrder(OrderId);
 		       if(foundOrder != null)
-			  System.out.println("Display all info:"+foundOrder.toString());
+			  System.out.println("Display all info for your order:"+foundOrder.toString());
 
 		       else 
 		         System.out.println("Your order is not available");
