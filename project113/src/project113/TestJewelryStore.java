ec0403329aec7ae7fd833f16304aa6deee8ebe77
njoud113 <161475859+njoud113@users.noreply.github.com>
2024-03-10 04:51:14 +0300

    Update TestJewelryStore.java

--- project113/src/project113/TestJewelryStore.java
+++ project113/src/project113/TestJewelryStore.java
@@ -1,28 +1,29 @@
 package project113;
 
+import java.util.Scanner;
 public class TestJewelryStore {
-
-	public static void main(String[] args) {
-
-		Scanner input=new Scanner(System.in);
+public static void main(String[] args) {
+      Scanner input=new Scanner(System.in);
     
-		JewelryStore OurStore= new JewelryStore("Cartier",100);
-		
-             int ch;
-		do{
-             System.out.println("**********Cartier store**********");
-             System.out.println("welcome to Cartier store customer service , what do you need today?(Enter your menu option)");
-             System.out.println("1. Add order for one customer");
-             System.out.println("2. Find a customer order");
-	    System.out.println("3. Delete a customer order");
-	     System.out.println("4. Display a customer order");
-	     System.out.println("5. Display today's orderes");
-	     System.out.println("6. Exit");
-	     System.out.println(" Enter your choice is:");
-             ch= input.nextInt();
-	      System.out.println("your choice is : "+ch);
-       switch(ch){
-              case 1:
+      JewelryStore OurStore =new JewelryStore("Cartier",100);
+      
+      int ch;//choose
+      do{
+      
+         System.out.println("**********Cartier store**********");
+         System.out.println("welcome to Cartier store customer service , what do you need today?(Enter your menu option)");
+         System.out.println("1. Add order for one customer");
+         System.out.println("2. Find a customer order");
+         System.out.println("3. Delete a customer order");
+         System.out.println("4. Display a customer order");
+         System.out.println("5. Display today's orderes");
+         System.out.println("6. Exit");
+         System.out.println(" Enter your choice is:");
+         ch= input.nextInt();
+         
+          System.out.println("your choice is : "+ ch);
+         switch(ch){
+            case 1:
                System.out.println("----------Add order----------");
                System.out.println("Enter customer name:");
                String CustomerName=input.next();
@@ -32,7 +33,7 @@ public class TestJewelryStore {
                double JewleryPrice=input.nextDouble();
                System.out.println("Enter order id:");
                String OrderId=input.next();
-                       if(JewleryPrice >=25000){
+              if(JewleryPrice >=25000){
 			     Customer customerG=new Golden(CustomerName,CustomerPassword,JewleryPrice);
 			     Order ord=new Order(OrderId,customerG);
                OurStore.addOrder(ord);  }
@@ -41,65 +42,62 @@ public class TestJewelryStore {
 			     Customer customerS=new Silver(CustomerName,CustomerPassword,JewleryPrice); 
 		        Order ord=new Order(OrderId,customerS);
 	           OurStore.addOrder(ord);}
-                
-
-           
-     
                break;
+               
+               
             case 2:
-            System.out.println("----------Find order----------");
-            System.out.println("Enter order id:");
-               String OrderId=input.next();
-        Order foundOrder=Order OurStore.searchOrder(OrderId);
+             System.out.println("----------Find order----------");
+             System.out.println("Enter order id first to find:");
+             OrderId=input.next();
+             Order foundOrder=OurStore.searchOrder(OrderId);// copy constructer?!
 		       if(foundOrder != null)
-			 System.out.println("Your order is available");
-		       else 
-		         System.out.println("Your order is not available");
+           System.out.println(" Your order is available");
+           else
+            System.out.println(" Your order is not  available");
                break;
-         
-            case 3:
-             System.out.println("----------Delete order----------");
-            System.out.println("Enter order id first to delete:");
-                OrderId=input.next();
-              if(OurStore.deleteOrder(OrderId)==true)
-		   System.out.println("The deletion was completed successfully");
-		       else System.out.println("Cannot delet order with this invalid id")
-	       break;
-
-		Case 4:
-                System.out.println("----------Display order----------");
-            System.out.println("Enter order id first to display:");
-                OrderId=input.next();
-
-	       foundOrder=Order OurStore.searchOrder(OrderId);
-		       if(foundOrder != null)
-			  System.out.println("Display all info for your order:"+foundOrder.toString());
-
-		       else 
-		         System.out.println("Your order is not available");
-		       
-
-		break;
-                 Case 5:
-System.out.println("----------Display today's orderes----------");
-              OurStore.display();
-
-	       break;
-
-		Case 6:
-System.out.println("****Goodbye****");
-
-
-		break;
-
-
-       }
-
-			
-		}while(ch!=6);
     
-		
-		
-	}
+    
+            case 3:
+              System.out.println("----------Delete order----------");
+              System.out.println("Enter order id first to delete :");
+              OrderId=input.next();
+              if(OurStore.deleteOrder(OrderId)==true)
+		        System.out.println("The deletion was completed successfully");
+		        else 
+              System.out.println("Cannot delete order with this invalid id");
+	           break;
+             
+         
+            case 4:
+            System.out.println("----------Display order----------");
+            System.out.println("Enter order id first to Display :");
+             OrderId=input.next();
+             foundOrder=OurStore.searchOrder(OrderId);// copy constructer?!
+		       if(foundOrder != null)
+            System.out.println("Display all info:\n"+foundOrder.toString());
+           else
+            System.out.println(" Your order is not available");
 
-}
+               break;
+            case 5:
+              System.out.println("----------Display today's orderes----------");
+              OurStore.display();
+      
+               break;
+            case 6:
+            System.out.println("**********Goodbye**********");
+               break;
+      default:
+         System.out.println("invalid choice enter from 1-6 :");
+         }
+      
+      }while(ch!=6);
+    
+   	
+   	
+   }
+
+}  
+      
+      
+      
